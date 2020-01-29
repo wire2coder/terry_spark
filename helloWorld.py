42bed5d535f77a011f375723d3aca436ee456529
Terry <tstric31@hadw5.maas>
2020-01-29 22:00:41 +0000

    got the results of hellWorld.py written to a file

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,7 @@
 import pyspark
 
 sc = pyspark.SparkContext('local[*]')
-sc.setLogLevel('WARN')
+sc.setLogLevel('WARN') # this line is a life saver!
 
 txt = sc.textFile('file:////usr/share/doc/python/copyright')
 print("terry was here!!!")
@@ -9,3 +9,10 @@ print(txt.count())
 
 python_lines = txt.filter(lambda line: 'python' in line.lower())
 print(python_lines.count())
+
+# saving the results into a file
+with open('helloWolrd_results1.txt', 'w') as file_obj:
+  file_obj.write('Number of lines: %s \n' % txt.count())
+  file_obj.write('Number of lines with python: %s \n' % python_lines.count() )
+
+  
