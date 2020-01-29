2f81dad20b12e5f811389aaa7c87085f1477bffe
Terry <tstric31@hadw5.maas>
2020-01-29 21:32:32 +0000

    added helloWordl.py and set 'WARN' to see the output

--- SimpleApp.py
+++ SimpleApp.py
@@ -1,21 +1,21 @@
 """SimpleApp.py"""
+from __future__ import print_function
 
 import sys
-
-from __future__ import print_function
 from operator import add
 from pyspark.sql import SparkSession
 
 if __name__ == "__main__":
 
-    logFile = "YOUR_SPARK_HOME/README.md"  # Should be some file on your system
+    logFile = "/user/tstric31/43-0.txt"  # Should be some file on your system
     spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
     logData = spark.read.text(logFile).cache()
 
-    numAs = logData.filter(logData.value.contains('a')).count()
-    numBs = logData.filter(logData.value.contains('b')).count()
+    # numAs = logData.filter(logData.value.contains('a')).count()
+    # numBs = logData.filter(logData.value.contains('b')).count()
 
-    print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
+
+    # print("Lines with a: %i, lines with b: %i" % (numAs, numBs))
     print("HELLJKLDJKLDFKLJKLFJD J>>>>>>>> ")
 
     spark.stop()
